93d87638f669bf91de2b03204c5e3ed229ea57ef
Blue Jessen <canealisblue@gmail.com>
2022-07-09 14:33:35 -0700

    Make all movies show on one screen

--- src/MoviesCard.js
+++ src/MoviesCard.js
@@ -2,10 +2,10 @@ import React, { Component }from "react";
 import './MoviesCard.css';
 
 const MoviesCard = (props) => {
-	return <div className='MoviesCard'>
+	return <div className='card'>
     <h1>{props.title}</h1>
     <h1>{props.rating}</h1>
-    <img src={props.imageUrl}></img>
+    <img className='movie-poster' src={props.imageUrl}></img>
   </div>;
 };
 
