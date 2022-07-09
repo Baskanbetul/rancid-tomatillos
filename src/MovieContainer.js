ba273932c6682b1a60a4838e575b9061ac36677b
Baskanbetul <btlbskn@hotmail.com>
2022-07-09 11:40:49 -0600

    Add functional and class components

---
+++ src/MovieContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MoviesCard from './MoviesCard.js';
+
+const MovieContainer = () => {
+
+  return (
+    <h1>Movies</h1>
+  )
+
+}
+
+export default MovieContainer;
---
+++ src/MoviesCard.js
@@ -0,0 +1,17 @@
+import React, { Component }from "react";
+import './MoviesCard.css';
+
+class MoviesCard extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+
+  }
+}
+
+export default MoviesCard;
