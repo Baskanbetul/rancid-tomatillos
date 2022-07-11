d561e87c4b4d956b06e17636b97427d854c1ee0d
Blue Jessen <canealisblue@gmail.com>
2022-07-11 00:20:28 -0700

    Add all details of movie

--- src/FocusCard.js
+++ src/FocusCard.js
@@ -8,15 +8,17 @@ const FocusCard = ({movie}) => {
     		<img className='movie-poster-large' src={movie.poster_path}></img>
 				<div className='movie-overview'>
 					<h1>{movie.title}</h1>
-					<p> Movie overview</p>
+					<p>{movie.overview}</p>
 					<div> Movie TRAILER </div>
 				</div>
 				<div className='movie-specs'>
-					<h1 className='details'>Movie Genre</h1>
-					<h1 className='details'>{movie.release_date}</h1>
-					<h1 className='details'> Movie Runtime</h1>
+					<h1 className='details'>{movie.genre}</h1>
+					<h1 className='details'>Released: {movie.release_date}</h1>
+					<h1 className='details'>Runtime: {movie.runtime} minutes</h1>
+					<h1 className='details'>Budget: {movie.budget}</h1>
+					<h1 className='details'>Revenue: {movie.revenue}</h1>
 					<div>
-					<h1 className='movie-rating details'>{movie.average_rating.toFixed(1)}</h1>
+					<h1 className='details'>Rating: {movie.average_rating.toFixed(1)}</h1>
 					</div>
 				</div>
   		</div>);
