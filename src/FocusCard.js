72bd954788d64ba6d2993bb20fe6ae406c38c15b
Blue Jessen <canealisblue@gmail.com>
2022-07-11 00:27:31 -0700

    Make backdrop the whole focus screen

--- src/FocusCard.js
+++ src/FocusCard.js
@@ -3,22 +3,24 @@ import './FocusCard.css'
 
 const FocusCard = ({movie}) => {
 		return (
-			<div className='focus-card' style={{backgroundImage: `url(${movie.backdrop_path})`,
+			<div className='background' style={{backgroundImage: `url(${movie.backdrop_path})`,
 			backgroundRepeat: "no-repeat", backgroundSize: 'cover',}}>
-    		<img className='movie-poster-large' src={movie.poster_path}></img>
-				<div className='movie-overview'>
-					<h1>{movie.title}</h1>
-					<p>{movie.overview}</p>
-					<div> Movie TRAILER </div>
-				</div>
-				<div className='movie-specs'>
-					<h1 className='details'>{movie.genre}</h1>
-					<h1 className='details'>Released: {movie.release_date}</h1>
-					<h1 className='details'>Runtime: {movie.runtime} minutes</h1>
-					<h1 className='details'>Budget: {movie.budget}</h1>
-					<h1 className='details'>Revenue: {movie.revenue}</h1>
-					<div>
-					<h1 className='details'>Rating: {movie.average_rating.toFixed(1)}</h1>
+				<div className= 'focus-card'>
+	    		<img className='movie-poster-large' src={movie.poster_path}></img>
+					<div className='movie-overview'>
+						<h1>{movie.title}</h1>
+						<p>{movie.overview}</p>
+						<div> Movie TRAILER </div>
+					</div>
+					<div className='movie-specs'>
+						<h1 className='details'>{movie.genre}</h1>
+						<h1 className='details'>Released: {movie.release_date}</h1>
+						<h1 className='details'>Runtime: {movie.runtime} minutes</h1>
+						<h1 className='details'>Budget: {movie.budget}</h1>
+						<h1 className='details'>Revenue: {movie.revenue}</h1>
+						<div>
+						<h1 className='details'>Rating: {movie.average_rating.toFixed(1)}</h1>
+						</div>
 					</div>
 				</div>
   		</div>);
