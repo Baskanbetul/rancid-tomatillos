c3815bf04ae9fc0f9dd064ee17e559d6d840f167
Blue Jessen <canealisblue@gmail.com>
2022-07-12 13:16:56 -0700

    Clean up Movie Trailer function code

--- src/MovieTrailer.js
+++ src/MovieTrailer.js
@@ -2,25 +2,17 @@ import React from 'react'
 import ReactPlayer from 'react-player';
 
 const MovieTrailer = ({trailer}) => {
-    if(!trailer) {
-      return
-    }
+    if(!trailer) {return}
   const findTrailer = trailer.find(video => {
     return video.type === "Trailer"
   })
-
-  console.log(findTrailer, 'TRAILER')
   let url = ''
-  if(findTrailer.site === 'Youtube') {
-    url = `https://www.youtube.com/watch?v=${findTrailer.key}`
-  } else {
+  findTrailer.site === 'Youtube' ?
+    url = `https://www.youtube.com/watch?v=${findTrailer.key}` :
     url = `https://www.vimeo.com/${findTrailer.key}`
-  }
+
   return (
-    // HERE
-    <div>
         <ReactPlayer url={`https://www.youtube.com/watch?v=${findTrailer.key}`} />
-    </div>
   )
     }
 
