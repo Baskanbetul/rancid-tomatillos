38d677cb78b6288f1157eb3f5003f33c2c4468f8
Blue Jessen <canealisblue@gmail.com>
2022-07-11 17:50:35 -0700

    Add happy path get test

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -7,6 +7,13 @@ it('Moldy Pears should be the header', () => {
   cy.contains('Moldy Pears')
 })
 
+it('User should see all movies', () => {
+  cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+    statusCode: 200
+  })
+  .get('div').should('have.class', 'card')
+})
+
 it('Should show error message if server is down', () => {
   cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
     statusCode: 500
