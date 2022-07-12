8b2445f1a98b8fe4e7fb1c95c49783796061142e
Blue Jessen <canealisblue@gmail.com>
2022-07-12 13:14:26 -0700

    Add error handling for vimeo and no videos

--- src/MovieTrailer.js
+++ src/MovieTrailer.js
@@ -2,10 +2,20 @@ import React from 'react'
 import ReactPlayer from 'react-player';
 
 const MovieTrailer = ({trailer}) => {
-
+    if(!trailer) {
+      return
+    }
   const findTrailer = trailer.find(video => {
     return video.type === "Trailer"
   })
+
+  console.log(findTrailer, 'TRAILER')
+  let url = ''
+  if(findTrailer.site === 'Youtube') {
+    url = `https://www.youtube.com/watch?v=${findTrailer.key}`
+  } else {
+    url = `https://www.vimeo.com/${findTrailer.key}`
+  }
   return (
     // HERE
     <div>
