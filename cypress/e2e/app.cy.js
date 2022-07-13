03b1adaddbbaa562464c16b5da25d06425448415
Blue Jessen <canealisblue@gmail.com>
2022-07-13 14:49:59 -0700

    Add test to make sure dashboard displays correctly

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,23 +1,27 @@
-// import './fixtures/.movies.json' 
 
 describe('App', () => {
   beforeEach('The user should see the main page', () => {
-    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
-    statusCode: 200,
-    fixture: "movies.json" 
-  })  
+    cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+    fixture: "movies.json",
+    statusCode: 200
+  })
+  cy.visit('http://localhost:3000/')
 })
 
 
 it('Moldy Pears should be the header', () => {
-  cy.visit('http://localhost:3000/');
-  cy.contains('Moldy Pears')
+  cy.get('header').within(() => {
+    cy.get('button').contains('Moldy Pears')
+  })
 })
 
 it('User should see all movies', () => {
-  cy.get('div').should('have.class', 'card').should('have.length',6)
+  cy.get('.movie-container').within(() => {
+    cy.get('.card').should('have.length', 6)
+    cy.get('.card').eq(0).contains('Money Plane')
+    cy.get('.card').eq(2).find('.movie-poster').should('have.attr', 'src', 'https://image.tmdb.org/t/p/original//uOw5JD8IlD546feZ6oxbIjvN66P.jpg')
   })
-
+})
 
 it('Should show error message if server is down', () => {
   cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
