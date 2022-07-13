dd24e7e4bc2ae64f3c4d6773709956136980035a
Blue Jessen <canealisblue@gmail.com>
2022-07-13 15:19:43 -0700

    Set up focus card test

--- cypress/e2e/focus-card.cy.js
+++ cypress/e2e/focus-card.cy.js
@@ -1,5 +1,16 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Focus Card', () => {
+  beforeEach('The user should see the main page', () => {
+    cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+    fixture: "movies.json",
+    statusCode: 200
   })
+  cy.visit('http://localhost:3000/')
+  cy.get('.movie-container').within(() => {
+    cy.get('.card').eq(1).click()
+  })
+})
+
+it('Should have the Moldy Pears Header', () => {
+  cy.get('header').contains('Moldy Pears')
+})
 })
