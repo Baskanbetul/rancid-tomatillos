cfd88c521c2cc193d7ef9f8fa6f993335d703979
Baskanbetul <btlbskn@hotmail.com>
2022-07-13 16:53:31 -0600

    Add test to check movie header, poster,title,released,runtime,budget,revenue and rating

--- cypress/e2e/focus-card.cy.js
+++ cypress/e2e/focus-card.cy.js
@@ -13,4 +13,32 @@ describe('Focus Card', () => {
 it('Should have the Moldy Pears Header', () => {
   cy.get('header').contains('Moldy Pears')
 })
+
+it('Should see movie poster and title about movie after click it', () => {
+  cy.get('.focus-card').within(() => {
+    cy.get('.movie-poster-large').should('have.attr', 'src', 'https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg');
+    cy.contains('Mulan'); 
+  })
 })
+
+it('Should see extra info about movie after click it', () => {
+  cy.get('.movie-specs').within(() => {
+		cy.get('.details').eq(1).contains('Released: 2020-09-04');
+		cy.get('.details').eq(2).contains('Runtime: 115');
+		cy.get('.details').eq(3).contains('Budget: 200000000');
+		cy.get('.details').eq(4).contains('Revenue: 57000000');
+    cy.get('.details').eq(5).contains('Rating: 5.1');
+	});
+})
+
+})
+
+//click moldpear go back to main page 
+
+
+	// "id": 337401,
+	// 		"poster_path": "https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg",
+	// 		"backdrop_path": "https://image.tmdb.org/t/p/original//zzWGRw277MNoCs3zhyG3YmYQsXv.jpg",
+	// 		"title": "Mulan",
+	// 		"average_rating": 4.909090909090909,
+	// 		"release_date": "2020-09-04"
